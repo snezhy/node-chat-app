14e818e098ab7f0d30bee0536ed9c65e4b19059a
Snezhana <snezhy@rocketmail.com>
2018-12-01 18:08:05 +0000

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,14 +13,33 @@ let io = socketIO(server);
 io.on('connection', (socket) => {
     console.log("New user connected");
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('Message Created', message);
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    //     });
+    // });
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
